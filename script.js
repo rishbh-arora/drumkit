bb8c79381f1a660f210766941c76ab137ea11cea
Rishbh Arora <vsryarora6879@gmail.com>
2023-08-29 14:19:14 +0530

    volume control

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-    const elements = ["bass", "snare","hihat", "crash", "high", "mid", "ride", "floor"]
-    const music = [new Audio("./sounds/kick.mp3"), new Audio("./sounds/snare.mp3"), new Audio("./sounds/hihat.mp3"),new Audio("./sounds/crash.mp3"), new Audio("./sounds/high-tom.mp3"), new Audio("./sounds/mid-tom.mp3"),new Audio("./sounds/ride.mp3"),new Audio("./sounds/floor.mp3"), ]
-    const keys = ['x','c','s','d','f','w','e','r']
+const elements = ["bass", "snare","hihat", "crash", "high", "mid", "ride", "floor"]
+const music = [new Audio("./sounds/kick.mp3"), new Audio("./sounds/snare.mp3"), new Audio("./sounds/hihat.mp3"),new Audio("./sounds/crash.mp3"), new Audio("./sounds/high-tom.mp3"), new Audio("./sounds/mid-tom.mp3"),new Audio("./sounds/ride.mp3"),new Audio("./sounds/floor.mp3"), ]
+const keys = ['x','c','s','d','f','w','e','r']
 
+let vol = 1;
 
-    for (let i = 0; i < elements.length; i++) {
-        document.getElementById(elements[i]).addEventListener("click", () => {
+for (let i = 0; i < elements.length; i++) {
+    document.getElementById(elements[i]).addEventListener("click", () => {
+        music[i].currentTime = 0;
+        music[i].volume = vol
+        music[i].play();
+        });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key.toLowerCase() === keys[i]) {
             music[i].currentTime = 0;
+            music[i].volume = vol
             music[i].play();
-            console.log("Event added")
-            });
+        }
+        });
+}
 
-        document.addEventListener('keydown', (e) => {
-            if (e.key.toLowerCase() === keys[i]) {
-                music[i].currentTime = 0;
-                music[i].play();
-            }
-            });
-    }
+function vol_control() {
+    vol = document.getElementById("vol").value/100;
+}
