cdf107194f60e79a2f30cead62ab4a278cd03744
Rishbh Arora <vsryarora6879@gmail.com>
2023-08-29 13:24:46 +0530

    added key binding

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const elements = ["bass", "snare","hihat", "crash", "high", "mid", "ride", "floor"]
-const music = [new Audio("./sounds/kick.mp3"), new Audio("./sounds/snare.mp3"), new Audio("./sounds/hihat.mp3"),new Audio("./sounds/crash.mp3"), new Audio("./sounds/high-tom.mp3"), new Audio("./sounds/mid-tom.mp3"),new Audio("./sounds/ride.mp3"),new Audio("./sounds/floor.mp3"), ]
+    const elements = ["bass", "snare","hihat", "crash", "high", "mid", "ride", "floor"]
+    const music = [new Audio("./sounds/kick.mp3"), new Audio("./sounds/snare.mp3"), new Audio("./sounds/hihat.mp3"),new Audio("./sounds/crash.mp3"), new Audio("./sounds/high-tom.mp3"), new Audio("./sounds/mid-tom.mp3"),new Audio("./sounds/ride.mp3"),new Audio("./sounds/floor.mp3"), ]
+    const keys = ['x','c','s','d','f','w','e','r']
 
-for (let i = 0; i < elements.length; i++) {
-    document.getElementById(elements[i]).addEventListener("click", () => {
-        music[i].play();
-        console.log("Event added")
-        });
 
-    document.getElementById(elements[i]).addEventListener('keydown', (e) => {
-        if (e.key.toLowerCase() === 'd') {
+    for (let i = 0; i < elements.length; i++) {
+        document.getElementById(elements[i]).addEventListener("click", () => {
+            music[i].currentTime = 0;
             music[i].play();
-        }
-        });
+            console.log("Event added")
+            });
 
-}
+        document.addEventListener('keydown', (e) => {
+            if (e.key.toLowerCase() === keys[i]) {
+                music[i].currentTime = 0;
+                music[i].play();
+            }
+            });
+    }
